32d02e3c86866ad6366741ab561bbb1e5f3c6cf2
Jeremiah Garza <jag_tt@yahoo.com>
2019-09-13 16:21:24 -0500

    update static

--- milo/main_app/urls.py
+++ milo/main_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('profile', views.profile, name='profile'),
+    path('profile/', views.profile, name='profile'),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
