b89a95635fe91ee5b9c5fa16d273fc4ca87b74d1
Elco Garcia <garciaelco@gmail.com>
2019-09-13 16:47:45 -0500

    added sign up url

--- milo/main_app/urls.py
+++ milo/main_app/urls.py
@@ -4,12 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-<<<<<<< HEAD
-    path('profile', views.profile, name='profile'),
+    path('profile/', views.profile, name='profile'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup',views.signup,name='signup')
-=======
-    path('profile/', views.profile, name='profile'),
-    path('accounts/', include('django.contrib.auth.urls'))
->>>>>>> 32d02e3c86866ad6366741ab561bbb1e5f3c6cf2
 ]
