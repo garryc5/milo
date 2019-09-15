e67e6d87139db9ce90de4eebea5a2f5442687b8e
Elco Garcia <garciaelco@gmail.com>
2019-09-15 13:24:41 -0500

    added photos function/views

--- milo/main_app/views.py
+++ milo/main_app/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+
+#login imports
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+#imports to upload pics
+import uuid
+import boto3
+from .models import Profile, Photo
+
+S3_BASE_URL = 'https://s3.us-east-2.amazonaws.com'
+BUCKET= 'miloprofilepics'
 
 
 # Create your views here.
@@ -19,15 +28,34 @@ def profile(request):
 # Need to add step 8 in authentication to the prfile create view
 
 def signup(request):
-    error_message = ''
-    if request.method == 'POST':
-     form = UserCreationForm(request.POST)
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
     if form.is_valid():
+      # This will add the user to the database
       user = form.save()
+      # This is how we log a user in via code
       login(request, user)
-      return redirect('index')
+      return redirect('profile')
     else:
       error_message = 'Invalid sign up - try again'
-    form = UserCreationForm()
-    context = {'form': form, 'error_message': error_message}
-    return render(request, 'registration/signup.html', context)
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
+
+def add_photo(request,profile_id):
+    photo_file = request.FILES.get('photo-file',None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            photo = Photo(url=url, cat_id=profile_id)
+            photo.save()
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('detail', cat_id=profile_id)
