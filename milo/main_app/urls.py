ed38f7c67df58cb0d74c1d8101001ca3b2f6217f
Elco Garcia <garciaelco@gmail.com>
2019-09-15 13:23:34 -0500

    added url path to add photos

--- milo/main_app/urls.py
+++ milo/main_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('profile/', views.profile, name='profile'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/signup',views.signup,name='signup')
+    path('accounts/signup',views.signup,name='signup'),
+    #path('profile/<int:profile_id>/add_photo/', views.add_photo, name='add_photo')
 ]
