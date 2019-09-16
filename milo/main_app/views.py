cead5753deecde0f0c8dfca40d41ed5bb14c7489
gcarver <gcarver@gcarvers-MacBook-Pro.local>
2019-09-16 10:29:46 -0500

    work page

--- milo/main_app/views.py
+++ milo/main_app/views.py
@@ -17,22 +17,19 @@ def profile(request):
 
 
 
-# Need to add step 8 in authentication to the prfile create view
-
 def signup(request):
-    error_message = ''
-    if request.method == 'POST':
-     form = UserCreationForm(request.POST)
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
     if form.is_valid():
       user = form.save()
       login(request, user)
-      return redirect('index')
+      return redirect('profile')
     else:
       error_message = 'Invalid sign up - try again'
-    form = UserCreationForm()
-    context = {'form': form, 'error_message': error_message}
-    return render(request, 'registration/signup.html', context)
-
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
 
 
 
