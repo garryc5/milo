c943e3fcccd4b025737ecd664912ed35a6e1fd1a
Elco Garcia <garciaelco18@gmail.com>
2019-09-18 10:58:20 -0500

    code to upload photos

--- milo/main_app/urls.py
+++ milo/main_app/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('profile/', views.profile, name='profile'),
     path('profile/create/', views.ProfileCreate.as_view(), name='profile_create'),
-     path('profile/<int:pk>/update/', views.ProfileUpdate.as_view(), name='profile_update'),
+    path('profile/<int:pk>/update/',
+         views.ProfileUpdate.as_view(), name='profile_update'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/signup',views.signup,name='signup'),
-    #path('profile/<int:profile_id>/add_photo/', views.add_photo, name='add_photo')
+    path('accounts/signup', views.signup, name='signup'),
+    path('profile/<int:profile_id>/add_photo/',
+         views.add_photo, name='add_photo')
 ]
--- milo/main_app/views.py
+++ milo/main_app/views.py
@@ -2,76 +2,106 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView, UpdateView
-#login imports
+# login imports
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Profile
-
+from .models import Profile, Photo
 from bs4 import BeautifulSoup
 import requests
 
+# photo import
+import uuid
+import boto3
+
+S3_BASE_URL = 'https://s3.us-east-2.amazonaws.com'
+BUCKET = 'miloprofilepics'
 
 # Create your views here.
+
 
 def home(request):
     toReturn = webscrapper()
     return render(request, 'home.html',
-    {
-      'toReturn': toReturn
-    })
+                  {
+                      'toReturn': toReturn
+                  })
+
 
 def profile(request):
-    profile=Profile.objects.get(user_id = request.user.id)
+    profile = Profile.objects.get(user_id=request.user.id)
     return render(request, 'profile.html',
-    {
-      'profile' : profile,
-    }
-    )    
-
+                  {
+                      'profile': profile,
+                  }
+                  )
 
 
 def signup(request):
-  error_message = ''
-  if request.method == 'POST':
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      user = form.save()
-      login(request, user)
-      return redirect('../profile/create')
-    else:
-      error_message = 'Invalid sign up - try again'
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'registration/signup.html', context)
-
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('../profile/create')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
 
 
 def webscrapper():
-  url = "https://www.ideafit.com/fitness-library"
-  response = requests.get(url , timeout=5)
-  content = BeautifulSoup(response.content, "html.parser")
-  grabbedfeed = content.find_all("div", attrs={"class": "box white article teaser large wide nopad has-image clearfix"})
-  toReturn = []
-  for x in range(6):
-    artical = {
-        'img' : grabbedfeed[x].img.get("src"),
-        'link' : url + grabbedfeed[x].h3.a.get('href'),
-        'headline' : grabbedfeed[x].h3.text,
-        'discript' : grabbedfeed[x].p.text
-    }
-    toReturn.append(artical)
-  return toReturn
+    url = "https://www.ideafit.com/fitness-library"
+    response = requests.get(url, timeout=5)
+    content = BeautifulSoup(response.content, "html.parser")
+    grabbedfeed = content.find_all("div", attrs={
+                                   "class": "box white article teaser large wide nopad has-image clearfix"})
+    toReturn = []
+    for x in range(6):
+        artical = {
+            'img': grabbedfeed[x].img.get("src"),
+            'link': url + grabbedfeed[x].h3.a.get('href'),
+            'headline': grabbedfeed[x].h3.text,
+            'discript': grabbedfeed[x].p.text
+        }
+        toReturn.append(artical)
+    return toReturn
+
 
 class ProfileCreate(CreateView):
-  model = Profile
-  fields = ['name','dob','picture','sport_bio','goal']
-  success_url='/profile/'
-  def form_valid(self, form):
-    form.instance.user = self.request.user
-    return super().form_valid(form)
+    model = Profile
+    fields = ['name', 'dob', 'picture', 'sport_bio', 'goal']
+    success_url = '/profile/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
 
 class ProfileUpdate(UpdateView):
-  model = Profile
-  fields= ['name','picture','sport_bio','goal']
-  sucess_url = '/profile/'
+    model = Profile
+    fields = ['name', 'picture', 'sport_bio', 'goal']
+    sucess_url = '/profile/'
+
+
+def add_photo(request, profile_id):
+    # photo-file will be the "name" attribute on the <input type="file">
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + \
+            photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            # we can assign to cat_id or cat (if you have a cat object)
+            photo = Photo(url=url, profile_id=profile_id)
+            photo.save()
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('/profile/')
