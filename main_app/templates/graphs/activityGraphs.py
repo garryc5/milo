360a668eb716ed0d2239b1533b5029be3dd727b8
gcarver <gcarver@gcarvers-MacBook-Pro.local>
2019-09-19 14:30:11 -0500

    fixxed/remove graphs

--- main_app/templates/graphs/activityGraphs.py
+++ main_app/templates/graphs/activityGraphs.py
@@ -1,39 +1,45 @@
-import matplotlib.pyplot as plt
-
-run = []
-arms = []
-legs = []
-core = []
-dater = []
-datea = []
-datel = []
-datec = []
-lw = 0
-cw = 0
-aw = 0
-for act in User.Activity:
-    if  act.activity = 'r':
-        run.append(act.rep)
-        dater.append(act.date)
-    elif act.activity = 'l':
-        legs.append(act.rep)
-        datel.append(act.date)
-        lw += act.weight
-    elif act.activity = 'c':
-        core.append(act.rep)
-        datec.append(act.date)
-        cw += act.weight
-    elif act.activity = 'a':
-        arms.append(act.rep)
-        datea.append(act.date)
-        aw += act.weight
- 
-plt.plot(dater,run,'ro',datea,arms,'bo',datec,core,'go',datel,legs,'yo')
-plt.ylabel('Reps / Distance')
-plt.xlabel('Date')
-console.log(plt.show());
-return 
-    {
-    graph :plt.show()
-    weights = f"arms:{aw}   legs:{lw}   core:{cw}"
-    }
+def activityParce()
+    run = ''
+    arms = ''
+    legs = ''
+    core = ''
+    dater = ''
+    datea = ''
+    datel = ''
+    datec = ''
+    lw = 0
+    cw = 0
+    aw = 0
+    for act in User.Activity:
+        if  act.activity = 'r':
+            run += f"Distance:  {act.rep}  "
+            dater += f"Date:  {act.date}  "
+        elif act.activity = 'l':
+            legs += f"Reps:  {act.rep}  "
+            datel += f"Date:  {act.date}  "
+            lw += act.weight
+        elif act.activity = 'c':
+            core += f"Reps:  {act.rep}  "
+            datec += f"Date:  {act.date}  "
+            cw += act.weight
+        elif act.activity = 'a':
+            arms += f"Reps:  {act.rep}  "
+            datea += f"Date:  {act.date}  "
+            aw += act.weight
+    
+    plt.plot(dater,run,'ro',datea,arms,'bo',datec,core,'go',datel,legs,'yo')
+    plt.ylabel('Reps / Distance')
+    plt.xlabel('Date')
+    console.log(plt.show());
+    return 
+        {
+        'run' : run,
+        'runDates':dater,
+        'legs': legs,
+        'legDates':datel,
+        'arms': arms,
+        'armDates':datea,
+        'core': core,
+        'coreDates':datec,
+        'weights' : f"arms:{aw}   legs:{lw}   core:{cw}"
+        }
