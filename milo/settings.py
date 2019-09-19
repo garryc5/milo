e3d4368031568db453fa138a6c7fcb3efcb6e37f
gcarver <gcarver@gcarvers-MacBook-Pro.local>
2019-09-19 14:39:06 -0500

    bix fix

--- milo/settings.py
+++ milo/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -123,3 +123,4 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/profile/'
 LOGOUT_REDIRECT_URL = '/'
 
+django_heroku.settings(locals())
